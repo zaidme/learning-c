284106686f98f61bef220fd279cbea1ba1d859e3
zaid42005 <zaid.42005@gmail.com>
2024-12-18 22:17:49 -0500

    so book.h is the header file where u declare stuff, book.c is the implementations of the declarations in book.h, and finally main is where u run that shi

---
+++ src/book.c
@@ -0,0 +1,41 @@
+#include "book.h"
+
+struct Book* create_book(const char* title, const char* author, int year){
+    struct Book* book =(struct Book*)malloc(sizeof(struct Book));
+
+    strcpy(book->title, title);
+    strcpy(book->author,author);
+    book->year=year;
+    book->connection_count=0;
+
+    return book;
+    
+};
+
+int connect_books(struct Book* book1, struct Book* book2, int weight){
+    if(book1->connection_count>=4 || book2->connection_count>=4){
+        return -1;
+    }
+
+    book1->connections[book1->connection_count].weight = weight;
+    book2->connections[book2->connection_count].weight = weight;
+
+
+    book1->connections[book1->connection_count].book = book2;
+    book2->connections[book2->connection_count].book = book1;
+};
+
+void display_connections(const struct Book* book){
+    if(book->connection_count==0){
+        printf("no connections\n");
+    };
+
+    for(int i = 0 ; i < book->connection_count; i++){
+    printf("Connected to %s by %s, RI: %d\n",
+    book->connections[i].book->title,
+    book->connections[i].book->author,
+    book->connections[i].weight);
+
+    
+    }
+}
--- src/book.h
+++ src/book.h
@@ -22,44 +22,11 @@ struct Book
     int connection_count;//used for appending new connections since u cant really do that as easily in c
 };
 
-struct Book* create_book(const char* title, const char* author, int year){
-    struct Book* book =(struct Book*)malloc(sizeof(struct Book));
-
-    strcpy(book->title, title);
-    strcpy(book->author,author);
-    book->year=year;
-    book->connection_count=0;
-
-    return book;
-    
-};
-
-int connect_books(struct Book* book1, struct Book* book2, int weight){
-    if(book1->connection_count>=4 || book2->connection_count>=4){
-        return -1;
-    }
-
-    book1->connections[book1->connection_count].weight = weight;
-    book2->connections[book2->connection_count].weight = weight;
+//declaring the functions for the header file
+struct Book* create_book(const char* title, const char* author, int year);
+int connect_books(struct Book* book1, struct Book* book2, int weight);
+void display_connections(const struct Book* book);
 
 
-    book1->connections[book1->connection_count].book = book2;
-    book2->connections[book2->connection_count].book = book1;
-};
-
-void display_connections(const struct Book* book){
-    if(book->connection_count==0){
-        printf("no connections\n");
-    };
-
-    for(int i = 0 ; i < book->connection_count; i++){
-    printf("Connected to %s by %s, RI: %d\n",
-    book->connections[i].book->title,
-    book->connections[i].book->author,
-    book->connections[i].weight);
-
-    
-    }
-}
 
 #endif
